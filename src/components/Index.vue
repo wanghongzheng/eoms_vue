fabc80700c175e91496f60cde2e3bdc373d9d4fe
wanghongzheng <wanghongzheng@712.cn>
2019-11-06 11:15:50 +0800

    功能列表项左上角数字标注，表示待处理的任务个数

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -14,36 +14,42 @@
                         <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
                             <a class="grid-content bg-purple" v-on:click="loadPage('故障查询1')">
                                 <el-image :src=faultImage></el-image>
+                                <label class="number"><span>1</span></label>
                                 <label>故障查询1</label>
                             </a>
                         </el-col>
                         <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
                             <a class="grid-content bg-purple" v-on:click="loadPage('故障查询2')">
                                 <el-image :src=faultImage></el-image>
+                                <label class="number"><span>1</span></label>
                                 <label>故障查询2</label>
                             </a>
                         </el-col>
                         <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
                             <a class="grid-content bg-purple" v-on:click="loadPage('故障查询3')">
                                 <el-image :src=faultImage></el-image>
+                                <label class="number"><span>1</span></label>
                                 <label>故障查询3</label>
                             </a>
                         </el-col>
                         <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
                             <a class="grid-content bg-purple" v-on:click="loadPage('故障查询4')">
                                 <el-image :src=faultImage></el-image>
+                                <label class="number"><span>99+</span></label>
                                 <label>故障查询4</label>
                             </a>
                         </el-col>
                         <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
                             <a class="grid-content bg-purple" v-on:click="loadPage('故障查询5')">
                                 <el-image :src=faultImage></el-image>
+                                <label class="number"><span>10</span></label>
                                 <label>故障查询5</label>
                             </a>
                         </el-col>
                         <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
                             <a class="grid-content bg-purple" v-on:click="loadPage('故障查询6')">
                                 <el-image :src=faultImage></el-image>
+                                <label class="number"><span>1</span></label>
                                 <label>故障查询6</label>
                             </a>
                         </el-col>
@@ -100,6 +106,25 @@ export default {
             width: 106px;
             text-align: center;
             margin-bottom: 15px;;
+        }
+        .number{
+            width: 25px;
+            height: 25px;
+            background-color: red;
+            margin-top: -100px;
+            margin-left: 90px;
+            z-index: 10000000;
+            position: absolute;
+            -moz-border-radius: 50%;
+            -webkit-border-radius: 50%;
+            span{
+                padding-top: 4px;
+                text-align: center;
+                vertical-align: bottom;
+                display: block;
+                color: #fff;
+                font-weight: bold;
+            }
         }
     }
 .head{
