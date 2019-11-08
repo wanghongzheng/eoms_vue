821c11380c2b2591f01b95db63c2db3c62acc1c0
wanghongzheng <wanghongzheng@712.cn>
2019-11-08 10:17:21 +0800

    故障处理

--- src/view/FaultHandle.vue
+++ src/view/FaultHandle.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="head">
             <el-button style="padding-right: 4px" type="text"
-                       v-on:click="goBack">
+                v-on:click="goBack">
                 <i class="el-icon-back" style="color:#fff;"></i>
             </el-button>
             <label>故障处理</label>
@@ -30,39 +30,32 @@
             </div>
             <div class="baseInfo">
                 <label>故障信息</label>
-                <el-upload
-                    action="#"
-                    list-type="picture-card"
-                    :auto-upload="false">
-                    <i slot="default" class="el-icon-plus"></i>
-                    <div slot="file" slot-scope="{file}">
-                        <img
-                            class="el-upload-list__item-thumbnail"
-                            :src="file.url" alt="">
-                        <span class="el-upload-list__item-actions">
-                            <span
-                                class="el-upload-list__item-preview"
-                                @click="handlePictureCardPreview(file)">
-                              <i class="el-icon-zoom-in"></i>
-                             </span>
-                            <span
-                                v-if="!disabled"
-                                class="el-upload-list__item-delete"
-                                @click="handleDownload(file)">
-                              <i class="el-icon-download"></i>
-                            </span>
-                            <span
-                                v-if="!disabled"
-                                class="el-upload-list__item-delete"
-                                @click="handleRemove(file)">
-                              <i class="el-icon-delete"></i>
-                            </span>
-                        </span>
-                    </div>
-                </el-upload>
-                <el-dialog :visible.sync="dialogVisible">
-                    <img width="100%" :src="dialogImageUrl" alt="">
-                </el-dialog>
+                <div style="margin-top:10px;">
+                    <el-upload
+                            action="auto"
+                            :http-request="uploadSectionFile"
+                            list-type="picture-card"
+                            :on-preview="handlePictureCardPreview"
+                            :on-remove="handleRemove"
+                            :on-change="handleLimit"
+
+                            :limit="3"
+                            multiple
+                            :class="{disabled:uploadDisabled}"
+                            ref="upload"
+
+                    >
+                        <i class="el-icon-plus" style="width:96px;height:96px;"></i>
+                    </el-upload>
+                    <el-dialog :visible.sync="dialogPicVisible">
+                        <img height="100%" :src="dialogImageUrl" alt="" />
+                    </el-dialog>
+                </div>
+                <div style="margin-top:10px;">
+                    <el-button type="primary" @click="upload()">上  传 </el-button>
+                    <el-button type="primary" @click="handleUploadRoadClose()">取  消</el-button>
+                    <el-button type="primary">查看图片</el-button>
+                </div>
             </div>
         </div>
     </div>
@@ -73,7 +66,12 @@
             return {
                 dialogImageUrl: '',
                 dialogVisible: false,
-                disabled: false
+                disabled: false,
+                uploadFile: [],
+                thisTitle: "测试",
+                uploadDisabled:false,
+                dialogPicVisible: false
+
             };
         },
         components: {
@@ -87,28 +85,96 @@
             handleRemove(file) {
                 console.log(file);
             },
+            handleUploadRoadClose() {
+                this.currentDialogVisible = false;
+                this.uploadFile = [];
+                this.$refs.upload.clearFiles();
+                this.$refs.upload.uploadFiles.length = 0;
+                this.$emit("update:dialogVisible", this.currentDialogVisible);
+                this.$message.info("已取消文件上传！");
+            },
             handlePictureCardPreview(file) {
                 this.dialogImageUrl = file.url;
                 this.dialogVisible = true;
             },
-            handleDownload(file) {
-                console.log(file);
+            handleLimit(file,fileList){
+                if(fileList.length>=3){
+                    this.uploadDisabled = true;
+                }
+            },
+            uploadSectionFile(param) {
+                var uploadFileLength = this.uploadFile.length;
+
+                let fileObj = param.file;
+
+                if (fileObj.type === "image/jpeg") {
+                    this.uploadFile[uploadFileLength] = {
+                        imgFile: fileObj
+                    };
+                } else if (fileObj.type === "image/png") {
+                    this.uploadFile[uploadFileLength] = {
+                        imgFile: fileObj
+                    };
+                } else {
+                    this.$message.error("只能上传jpg/png文件");
+                    return;
+                }
+            },
+            upload() {
+                var that = this;
+                that.$refs.upload.clearFiles();
+                if (this.uploadFile.length == 0) {
+                    this.$message.warning("请选择上传图片！");
+                    return false;
+                }
+                for (var int = 0; int < this.uploadFile.length; int++) {
+                    var param = new FormData(); // FormData 对象
+                    var list = this.uploadFile[int];
+                    var file = list.imgFile;
+                    param.append("fileType", "traffic");
+                    param.append("myFiles", file);
+                    param.append("dataId", "1111");
+                    param.append("deviceId", "222");
+                    param.append("stateCode", "1");
+                    this.uploadDisabled = false;
+                 /*   uploadPicFun(param).then(res => {
+                        if (res.success) {
+                            this.$message.success("文件上传成功！");
+                            that.currentDialogVisible = false;
+                            that.$emit("update:dialogVisible", false);
+                            this.uploadData.trafficId = res.data;
+                            this.$emit("spot", this.uploadData.trafficId);
+                        } else {
+                            this.$message.error("文件上传失败！");
+                            that.currentDialogVisible = true;
+                            that.$emit("update:dialogVisible", true);
+                        }
+                        that.$refs.upload.clearFiles();
+                    });*/
+                }
             }
         }
     }
 </script>
 <style lang="scss">
-    .el-upload .el-upload--picture-card{
+ .baseInfo .el-upload--picture-card{
         background-color: #fbfdff;
         border: 1px dashed #c0ccda;
         border-radius: 6px;
         -webkit-box-sizing: border-box;
         box-sizing: border-box;
-        width: 96px;
-        height: 96px;
-        line-height: 96px;
+        width: 110px;
+        height: 110px;
+        line-height: 110px;
         vertical-align: top;
     }
+ .baseInfo .el-upload-list--picture-card .el-upload-list__item{
+       width:110px;
+       height: 110px;
+   }
+   .disabled .el-upload--picture-card {
+       display: none!important;
+   }
 </style>
 <style scoped lang="scss">
     .head{
@@ -126,6 +192,7 @@
         clear: both;
         .title{
             float:left;
+            margin-top: 5px;
         }
         .op{
             float:right;
